1b17ed18ab5c3cee3d7db9dbab4894debc475ae1
Zeno Zeng <zenoofzeng@gmail.com>
2016-04-26 14:30:17 +0000

    path

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const _path = require('path');
-const projectRoot = _path.resolve(_path.dirname(__filename), '../..');
+const path = require('path');
 
 module.exports = function () {
     return {
         visitor: {
-            CallExpression: function CallExpression(path) {
-                var callee = path.node.callee;
+            CallExpression: function CallExpression(_path) {
+                var callee = _path.node.callee;
 
-                var modulePath = path.node.arguments[0];
+                var modulePath = _path.node.arguments[0];
                 var calleeIsRequire = callee.type == 'Identifier' && callee.name == 'require';
 
                 var isCloudModulePath = modulePath && modulePath.type == 'StringLiteral' && modulePath.value.indexOf('cloud/') === 0;
 
                 if (calleeIsRequire && isCloudModulePath) {
-                    var fromPath = path.hub.file.log.filename.split(_path.win32.sep).join(_path.posix.sep);
-                    var targetPath = _path.posix.join(projectRoot, modulePath.value);
-                    modulePath.value = _path.posix.relative(fromPath, targetPath);
+                    var fromPath = _path.hub.file.log.filename.split(path.win32.sep).join(path.posix.sep);
+                    var targetPath = modulePath.value;
+                    var relativePath = path.posix.relative(path.dirname(fromPath), targetPath);
+                    console.log({fromPath, targetPath, relativePath});
+                    modulePath.value = relativePath;
                 }
             }
         }
